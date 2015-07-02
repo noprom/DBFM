9e50168af595002da6744d807b1e445b95f897db
noprom <tyee.noprom@qq.com>
2015-07-02 19:45:45 +0800

    fix bugs

--- DBFM/ViewController.swift
+++ DBFM/ViewController.swift
@@ -9,27 +9,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelegate{
 
-    // 歌曲封面
+    //EkoImage组件，歌曲封面
     @IBOutlet weak var iv: EkoImage!
+    //歌曲列表
+    @IBOutlet weak var tv: UITableView!
     //背景
     @IBOutlet weak var bg: UIImageView!
-
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 旋转
         iv.onRotation()
-        
         //设置背景模糊
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.frame.size = CGSize(width: view.frame.width, height: view.frame.height)
         bg.addSubview(blurView)
-
-     
+        
+        //设置tbaleView的数据源和代理
+        tv.dataSource = self
+        tv.delegate = self
     }
-
+    //设置tableview的数据行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //配置tableView的单元格 cell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tv.dequeueReusableCellWithIdentifier("douban") as! UITableViewCell
+        //设置cell的标题
+        cell.textLabel?.text = "标题：\(indexPath.row)"
+        cell.detailTextLabel?.text = "子标题：\(indexPath.row)"
+        //设置缩略图
+        cell.imageView?.image = UIImage(named: "thumb")
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
