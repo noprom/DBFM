cc3d0461058523247564e1861c213fb9c830100d
noprom <tyee.noprom@qq.com>
2015-07-02 21:39:41 +0800

    channel delegate done

--- DBFM/ChannelController.swift
+++ DBFM/ChannelController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ChannelController: UIViewController {
-
+    
     // 频道列表
     @IBOutlet weak var tv: UITableView!
+    
+    // 申明代理
+    var detegate:ChannelController?
+    
+    // 频道列表的数据
+    var channelData:[JSON] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +26,17 @@ class ChannelController: UIViewController {
 
     // 配置数据源的行数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return channelData.count
     }
     
     // 配置cell的数据
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tv.dequeueReusableCellWithIdentifier("channel") as! UITableViewCell
+        // 获取行数据
+        let rowData:JSON = self.channelData[indexPath.row] as JSON
+        
         //设置cell的标题
-        cell.textLabel?.text = "频道：\(indexPath.row)"
+        cell.textLabel?.text = rowData["name"].string
         return cell
     }
     
@@ -35,8 +44,10 @@ class ChannelController: UIViewController {
         super.didReceiveMemoryWarning()
        
     }
-    
 
+}
 
-
+protocol ChannelProtocol{
+    // 回调方法，将频道id传回代理
+    func onChangeChannel(channelId:String)
 }
