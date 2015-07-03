c4f2354eec3d1eadd39e98fcd3e8beb6b5158a45
noprom <tyee.noprom@qq.com>
2015-07-03 20:20:19 +0800

    custom play button

---
+++ DBFM/EkoButton.swift
@@ -0,0 +1,36 @@
+//
+//  EkoButton.swift
+//  DBFM
+//
+//  Created by noprom on 15/7/3.
+//  Copyright (c) 2015年 noprom. All rights reserved.
+//
+
+import UIKit
+
+class EkoButton: UIButton {
+    
+    var isPlay:Bool = true
+    let imgPlay:UIImage = UIImage(named: "play")!
+    let imgPause:UIImage = UIImage(named: "pause")!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.addTarget(self, action: "onClick", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    func onClick(){
+        isPlay = !isPlay
+        if isPlay{
+            self.setImage(imgPause, forState: UIControlState.Normal)
+        }else{
+            self.setImage(imgPause, forState: UIControlState.Normal)
+        }
+    }
+    
+    func onPlay(){
+        isPlay = true
+        self.setImage(imgPause, forState: UIControlState.Normal)
+    }
+
+}
